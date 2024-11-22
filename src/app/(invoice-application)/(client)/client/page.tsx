e8759bdcd550dd3dfee4fd4349d4b47d4a9ff134
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-23 01:52:11 +0800

    Fix: Bugs in the Client and Invoices

--- src/app/(invoice-application)/(client)/client/page.tsx
+++ src/app/(invoice-application)/(client)/client/page.tsx
@@ -1,14 +1,27 @@
 "use client";
 
+import Button from "@/components/ui/Button";
+import useAuthentication from "@/hooks/useAuthentication";
 import React, { useEffect } from "react";
 
 const Client = () => {
+  useAuthentication();
+
   useEffect(() => {
     document.title = "Client - Invoice Application";
   });
+
   return (
     <>
-      <div>Client</div>
+      <div className="flex flex-col font-poppins gap-5 py-5 pl-4 pr-8 h-full w-full">
+        <h1 className="font-semibold text-[#262626] text-md lg:text-2xl">Client</h1>
+        <div>
+          <div className="flex justify-between">
+            <input className="border-2 border-[#EEEEEE] text-xs md:text-md lg:text-lg rounded-md pl-2 lg:pr-44" type="text" placeholder="Search" />
+            <Button>New Client</Button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/app/(invoice-application)/(invoices)/invoices/page.tsx
+++ src/app/(invoice-application)/(invoices)/invoices/page.tsx
@@ -1,14 +1,40 @@
 "use client";
 
+import Button from "@/components/ui/Button";
+import useAuthentication from "@/hooks/useAuthentication";
 import React, { useEffect } from "react";
 
 const Invoices = () => {
+  useAuthentication();
+
   useEffect(() => {
     document.title = "Invoices - Invoice Application";
   });
+
   return (
     <>
-      <div>Invoices</div>
+      <div className="flex flex-col font-poppins gap-5 h-full w-full">
+        <div className="flex justify-between items-center pt-5 pl-4 pr-8 w-full">
+          <h1 className="font-semibold text-md lg:text-2xl">Invoices</h1>
+          <Button>New Invoice</Button>
+        </div>
+        <hr className="border-[1px] border-solid w-full" />
+        <div className="flex flex-col lg:flex-row lg:justify-around items-center gap-4 pt-2 pb-5 pl-4 pr-8 w-full">
+          <div className="flex flex-col items-center">
+            <h1 className="font-semibold text-[#D2232D] text-md lg:text-2xl">₱4.5m PHP</h1>
+            <p className="text-[#BBBBBB] text-xs">Overdue</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <h1 className="font-semibold text-[#D2232D] text-md lg:text-2xl">₱4.5m PHP</h1>
+            <p className="text-[#BBBBBB] text-xs">Total Outstanding</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <h1 className="font-semibold text-[#D2232D] text-md lg:text-2xl">₱4.5m PHP</h1>
+            <p className="text-[#BBBBBB] text-xs">In Draft</p>
+          </div>
+        </div>
+        <hr className="border-[1px] border-solid ml-4 mr-8" />
+      </div>
     </>
   );
 };
