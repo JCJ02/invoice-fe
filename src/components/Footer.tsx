bbc33bd45401a9ec94ed4b19a1eb11589388eee5
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-23 01:52:32 +0800

    Fix: Adjust the Style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,11 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="fixed bottom-0 flex flex-col lg:flex-row justify-between items-center px-8 py-5 w-full">
-        <label className="font-poppins text-sm text-white">
+      <footer className="fixed bottom-0 flex flex-col lg:flex-row lg:justify-between items-center px-8 py-5 w-full">
+        <label className="font-poppins text-xs md:text-sm text-white">
           &copy; Copyright 2024 All Right Reserved.
         </label>
-        <div className="flex items-center font-poppins text-sm text-white gap-2">
+        <div className="flex items-center font-poppins text-xs md:text-sm text-white gap-2">
           <Link href={"*"}>Contact Us</Link>
           <label>|</label>
           <Link href={"*"}>Terms & Conditions</Link>
