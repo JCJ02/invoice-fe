f0740fab6da422ab9caf6e18645ff8609d3f63e6
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-23 19:58:38 +0800

    Feat: Style the Client

--- src/app/(invoice-application)/(client)/client/page.tsx
+++ src/app/(invoice-application)/(client)/client/page.tsx
@@ -14,10 +14,16 @@ const Client = () => {
   return (
     <>
       <div className="flex flex-col font-poppins gap-5 py-5 pl-4 pr-8 h-full w-full">
-        <h1 className="font-semibold text-[#262626] text-md lg:text-2xl">Client</h1>
+        <h1 className="font-semibold text-[#262626] text-md lg:text-2xl">
+          Client
+        </h1>
         <div>
           <div className="flex justify-between">
-            <input className="border-2 border-[#EEEEEE] text-xs md:text-md lg:text-lg rounded-md pl-2 lg:pr-44" type="text" placeholder="Search" />
+            <input
+              className="border-2 border-[#EEEEEE] text-xs md:text-md lg:text-lg rounded-md pl-2 lg:pr-44"
+              type="text"
+              placeholder="Search"
+            />
             <Button>New Client</Button>
           </div>
         </div>
