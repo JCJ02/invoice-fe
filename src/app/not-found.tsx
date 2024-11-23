b0437454d801c90dca6dd6f2eb67ed64a6fe9dee
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-23 19:57:55 +0800

    Feat: Add Not Found Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,35 @@
 "use client";
 
-import Link from 'next/link';
-import React, { useEffect } from 'react';
+import Link from "next/link";
+import React, { useEffect } from "react";
 
 const NotFound = () => {
-    useEffect(() => {
-        document.title = "404 Not Found - Invoice Application";
-    }, []);
-    return (
-        <>
-            <div className="flex flex-col justify-center items-center gap-2 font-poppins px-1 h-screen w-full">
-                <h1 className="font-bold text-5xl lg:text-8xl">404</h1>
-                <p className="text-center text-xs lg:text-lg">Oops... The Link You Clicked may be broken or the page may have been removed. We're Sorry!</p>
-            </div>
-        </>
-    )
-}
+  useEffect(() => {
+    document.title = "404 Not Found - Invoice Application";
+  }, []);
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-10 font-poppins px-8 m-auto h-screen max-w-[1280px]">
+        <h1 className="font-bold text-9xl lg:text-[216px]">404</h1>
+        <div className="flex flex-col items-center lg:items-start gap-5 lg:gap-2 w-full">
+          <div className="flex flex-col items-center lg:items-start gap-2 w-full">
+            <p className="font-bold text-xl">Oops!</p>
+            <p className="font-bold text-center lg:text-start text-xl">
+              The link you clicked may be broken or the page may have been
+              removed. We're Sorry!
+            </p>
+          </div>
+          <hr className="border-4 border-[#D1242E] my-5 w-1/2" />
+          <div className="flex justify-center items-center lg:justify-start lg:items-start gap-2 w-full">
+            <p className="font-bold text-xl">Go back to our </p>
+            <Link className="text-[#D1242E] font-bold text-xl" href={"/"}>
+              Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default NotFound;
