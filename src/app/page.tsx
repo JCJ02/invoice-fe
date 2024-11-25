c4f3e0d6dffd0936657820f9968ea529232c04d5
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-25 10:01:02 +0800

    Feat: Put Browser Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import useAuthentication from "@/hooks/useAuthentication";
+import { useEffect } from "react";
 
 export default function Home() {
   useAuthentication();
+
+  useEffect(() => {
+    document.title = "Invoice Application";
+  }, []);
   return <></>;
 }
