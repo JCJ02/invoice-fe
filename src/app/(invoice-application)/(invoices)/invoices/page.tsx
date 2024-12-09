c6291038978a02052c55e3381477d8718aa799f5
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-12-09 20:46:08 +0800

    Feat: Fetch Invoices Data in the Table

--- src/app/(invoice-application)/(invoices)/invoices/page.tsx
+++ src/app/(invoice-application)/(invoices)/invoices/page.tsx
@@ -3,7 +3,7 @@
 import Button from "@/components/Button";
 import SearchInput from "@/components/SearchInput";
 import useAuthentication from "@/hooks/useAuthentication";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -15,12 +15,44 @@ import {
 import { IoEyeOutline } from "react-icons/io5";
 import { FaRegEdit } from "react-icons/fa";
 import { MdDeleteOutline } from "react-icons/md";
+import useFetchInvoices from "./_hooks/useFetchInvoices";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import generatePaginationLinks from "@/utils/generatePaginationLinks";
 
 const Invoices = () => {
   useAuthentication();
 
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [limit] = useState(5);
+
+  const { data, isLoading, isError, error } = useFetchInvoices(
+    searchQuery,
+    currentPage,
+    limit
+  );
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+    setCurrentPage(1);
+  };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   useEffect(() => {
     document.title = "Invoices - Invoice Application";
+    console.log(
+      data && data.data.clients.filter((client) => client.invoices.length > 0)
+    );
   });
 
   return (
@@ -61,16 +93,16 @@ const Invoices = () => {
           <h1 className="font-semibold text-sm md:text-md lg:text-xl">
             All Invoices
           </h1>
-          <SearchInput />
+          <SearchInput value={searchQuery} onChange={handleSearchChange} />
         </div>
 
         {/* 4TH SECTION */}
-        <div className="pt-5 pl-4 pr-8 w-full">
+        <div className="flex flex-col gap-1 py-5 pl-4 pr-8 w-full">
           <Table>
             <TableHeader className="bg-[#D2232D]">
               <TableRow>
                 <TableHead className="text-white text-xs md:text-md">
-                  Client/Invoice Number
+                  Company Name/Invoice Number
                 </TableHead>
                 <TableHead className="text-white text-xs md:text-md">
                   Description
@@ -87,25 +119,130 @@ const Invoices = () => {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>Test</TableCell>
-                <TableCell>Test</TableCell>
-                <TableCell>Test</TableCell>
-                <TableCell>₱ 0.00 PHP</TableCell>
-                <TableCell className="flex items-center gap-1">
-                  <Button className="bg-white px-1 lg:px-1 py-1 text-black text-sm">
-                    <IoEyeOutline />
-                  </Button>
-                  <Button className="bg-white px-1 lg:px-1 py-1 text-black text-sm">
-                    <FaRegEdit />
-                  </Button>
-                  <Button className="bg-white px-1 lg:px-1 py-1 text-black text-sm">
-                    <MdDeleteOutline />
-                  </Button>
-                </TableCell>
-              </TableRow>
+              {isLoading ? (
+                <TableRow>
+                  <TableCell
+                    colSpan={5}
+                    className="text-center text-xs md:text-md lg:text-lg text-gray-500"
+                  >
+                    Loading...
+                  </TableCell>
+                </TableRow>
+              ) : isError ? (
+                <TableRow>
+                  <TableCell
+                    colSpan={5}
+                    className="text-center text-xs md:text-md lg:text-lg text-red-500"
+                  >
+                    {`Error: ${error?.message || "An Unknown Error Occurred."}`}
+                  </TableCell>
+                </TableRow>
+              ) : data && data?.data?.clients?.length > 0 ? (
+                data?.data?.clients?.map((client) =>
+                  client?.invoices?.length > 0
+                    ? client?.invoices?.map((invoice) => (
+                        <TableRow
+                          key={invoice.id}
+                          className={
+                            invoice.id % 2 === 0 ? "bg-[#FBE9EA]" : "bg-white"
+                          }
+                        >
+                          <TableCell className="flex flex-col gap-1">
+                            <label className="text-sm text-gray-500">
+                              {client.companyName}
+                            </label>
+                            <label className="text-sm">
+                              {invoice.invoiceNumber}
+                            </label>
+                          </TableCell>
+                          <TableCell>{invoice.description}</TableCell>
+                          <TableCell className="flex flex-col gap-1">
+                            <label className="text-sm">
+                              {new Date(
+                                invoice.issuedDate
+                              ).toLocaleDateString()}
+                            </label>
+                            <label className="text-sm">
+                              {new Date(invoice.dueDate).toLocaleDateString()}
+                            </label>
+                          </TableCell>
+                          <TableCell>
+                            ₱{" "}
+                            {parseFloat(
+                              invoice.totalOutstanding
+                            ).toLocaleString()}{" "}
+                            PHP
+                          </TableCell>
+                          <TableCell className="flex items-center gap-1">
+                            <Button className="bg-white px-1 lg:px-1 py-1 text-black text-sm">
+                              <IoEyeOutline />
+                            </Button>
+                            <Button className="bg-white px-1 lg:px-1 py-1 text-black text-sm">
+                              <FaRegEdit />
+                            </Button>
+                            <Button className="bg-white px-1 lg:px-1 py-1 text-black text-sm">
+                              <MdDeleteOutline />
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      ))
+                    : null
+                )
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={5}
+                    className="text-center text-xs md:text-md lg:text-lg text-gray-500"
+                    key={null}
+                  >
+                    No Data Found
+                  </TableCell>
+                </TableRow>
+              )}
             </TableBody>
           </Table>
+          {/* PAGINATION */}
+          <Pagination className="justify-end">
+            <PaginationContent className="flex items-center">
+              <PaginationItem>
+                <PaginationPrevious
+                  href="#"
+                  onClick={() => handlePageChange(currentPage - 1)}
+                  disabled={currentPage === 1}
+                  className="text-xs md:text-md lg:text-lg"
+                />
+              </PaginationItem>
+
+              {/* DYNAMIC PAGINATION LINKS */}
+              {generatePaginationLinks(
+                currentPage,
+                Math.ceil((data?.data?.totalClients ?? 0) / limit)
+              ).map((page) => (
+                <PaginationItem key={page}>
+                  <PaginationLink
+                    href="#"
+                    onClick={() => handlePageChange(page)}
+                    className={
+                      currentPage === page
+                        ? "bg-[#D2232D] text-white text-xs md:text-md lg:text-lg"
+                        : ""
+                    }
+                  >
+                    {page}
+                  </PaginationLink>
+                </PaginationItem>
+              ))}
+
+              <PaginationItem>
+                <PaginationNext
+                  href="#"
+                  onClick={() => handlePageChange(currentPage + 1)}
+                  disabled={data && data?.data?.clients?.length < limit}
+                  className="text-xs md:text-md lg:text-lg"
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </div>
     </>
