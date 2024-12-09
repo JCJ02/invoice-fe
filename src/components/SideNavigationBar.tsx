0193bf35c7e0512b836182740a1afb440429b8e1
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-12-09 20:42:11 +0800

    Fix: Adjust the Responsiveness of the SideNavigationBar component

--- src/components/SideNavigationBar.tsx
+++ src/components/SideNavigationBar.tsx
@@ -25,7 +25,7 @@ const SideNavigationBar = () => {
   const [isMobileView, setDesktopView] = useState(false);
 
   const responsiveView = () => {
-    setDesktopView(window.innerWidth < 1024);
+    setDesktopView(window.innerWidth < 1280);
   };
 
   useEffect(() => {
@@ -54,7 +54,7 @@ const SideNavigationBar = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center bg-white [box-shadow:10px_0_20px_0_rgba(0,0,0,0.1)] px-5 w-2/12 md:w-1/12 lg:w-[280px]">
+      <div className="flex flex-col items-center bg-white [box-shadow:10px_0_20px_0_rgba(0,0,0,0.1)] px-5 w-2/12 md:w-1/12 xl:w-[280px]">
         {/* LOGO */}
         {isMobileView ? (
           <Image
@@ -68,7 +68,7 @@ const SideNavigationBar = () => {
 
         {/* SIDE NAVIGATION BAR BUTTONS */}
         <div className="flex flex-col justify-between items-center h-full w-full">
-          <ul className="flex flex-col items-center lg:items-start gap-4 lg:px-5 w-full">
+          <ul className="flex flex-col items-center xl:items-start gap-4 xl:px-5 w-full">
             <li className="font-poppins text-lg">
               <TooltipProvider>
                 <Tooltip>
