65499b7d54d3339f6a108d5342ab6abc1943ab69
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-12-11 01:21:53 +0800

    Fix: Put CN For The Custom Style

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { cn } from "@/lib/utils";
 
 type InputSearchProps = {
+  className?: string;
   onChange?: any;
   name?: string;
   value?: any;
 };
 
-const SearchInput = ({ onChange, name, value }: InputSearchProps) => {
+const SearchInput = ({
+  className,
+  onChange,
+  name,
+  value,
+}: InputSearchProps) => {
   return (
     <input
-      className="border-2 border-[#EEEEEE] text-xs md:text-md lg:text-lg rounded-md pl-2 lg:pr-44"
+      className={cn(
+        "border-2 border-[#EEEEEE] text-xs md:text-md lg:text-lg rounded-md py-1 pl-2 lg:pr-44",
+        className
+      )}
       type="text"
       placeholder="Search"
       onChange={onChange}
