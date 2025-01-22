59faf7e5b24888fd3c99c43742a549b76182c32d
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-01-22 22:43:18 +0800

    Fix: Fetching invoices bug

--- src/app/(invoice-application)/(invoices)/invoices/_hooks/useFetchInvoices.ts
+++ src/app/(invoice-application)/(invoices)/invoices/_hooks/useFetchInvoices.ts
@@ -1,36 +1,37 @@
 import baseUrl from "@/utils/baseUrl";
 import useFetch from "@/hooks/useFetch";
+import { useRouter } from "next/navigation";
 
 interface Invoice {
-    id: number,
-    invoiceNumber: string;
-    clientId: number;
-    description: string;
-    rate: number;
-    quantity: number;
-    lineTotal: number;
-    issuedDate: string;
-    dueDate: string;
-    totalOutstanding: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deleteAt: Date;
+  id: number,
+  invoiceNumber: string;
+  clientId: number;
+  description: string;
+  rate: number;
+  quantity: number;
+  lineTotal: number;
+  issuedDate: string;
+  dueDate: string;
+  totalOutstanding: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deleteAt: Date;
 }
   
 interface Client {
-    id: number;
-    firstname: string;
-    lastname: string;
-    email: string;
-    companyName: string;
-    phoneNumber: string;
-    businessPhone: string;
-    mobilePhone: string;
-    address: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deleteAt: Date;
-    invoices: Invoice[];
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  companyName: string;
+  phoneNumber: string;
+  businessPhone: string;
+  mobilePhone: string;
+  address: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deleteAt: Date;
+  invoices: Invoice[];
 }
   
 interface InvoicesResponse {
@@ -43,15 +44,17 @@ interface InvoicesResponse {
 }
 
 const useFetchInvoices = (query: string, page: number, limit: number) => {
+    const router = useRouter();
     const token = localStorage.getItem("token");
   
     if (!token) {
+      router.push("/sign-in");
       throw new Error("Authorization Token is Missing!");
     }
   
     const { data, isLoading, isError, error } = useFetch<InvoicesResponse>(
       "invoices",
-      `${baseUrl}api/client/retrieve/invoice-list/`,
+      `${baseUrl}api/client/retrieve/client-with-invoice-list/`,
       {
         params: {
           query,
@@ -65,3 +68,56 @@ const useFetchInvoices = (query: string, page: number, limit: number) => {
 };
 
 export default useFetchInvoices;
+
+
+// import baseUrl from "@/utils/baseUrl";
+// import useFetch from "@/hooks/useFetch";
+
+// interface Invoice {
+//     id: number,
+//     invoiceNumber: string;
+//     clientId: number;
+//     description: string;
+//     rate: number;
+//     quantity: number;
+//     lineTotal: number;
+//     issuedDate: string;
+//     dueDate: string;
+//     totalOutstanding: string;
+//     createdAt: Date;
+//     updatedAt: Date;
+//     deleteAt: Date;
+// }
+  
+// interface InvoicesResponse {
+//     data: {
+//       invoices: Invoice[];
+//       totalInvoices: number;
+//     };
+//     message: string;
+//     code: number;
+// }
+
+// const useFetchInvoices = (query: string, page: number, limit: number) => {
+//     const token = localStorage.getItem("token");
+  
+//     if (!token) {
+//       throw new Error("Authorization Token is Missing!");
+//     }
+  
+//     const { data, isLoading, isError, error } = useFetch<InvoicesResponse>(
+//       "invoices",
+//       `${baseUrl}api/client/retrieve/invoice-list/`,
+//       {
+//         params: {
+//           query,
+//           page,
+//           limit,
+//         },
+//       }
+//     );
+  
+//     return { data, isLoading, isError, error };
+// };
+
+// export default useFetchInvoices;
